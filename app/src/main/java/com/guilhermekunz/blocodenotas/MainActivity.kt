b97e3e5f07ba78f752f5887b44b5ac00ddb5edce
guilherme-kunz <guilhermebutzenkunz@gmail.com>
2026-04-13 15:55:01 -0300

    MainActivity

--- app/src/main/java/com/guilhermekunz/blocodenotas/MainActivity.kt
+++ app/src/main/java/com/guilhermekunz/blocodenotas/MainActivity.kt
@@ -11,37 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.guilhermekunz.blocodenotas.presentation.notes.NotesScreen
 import com.guilhermekunz.blocodenotas.ui.theme.BlocoDeNotasTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BlocoDeNotasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NotesScreen(
+                    onAddNoteClick = { type ->
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BlocoDeNotasTheme {
-        Greeting("Android")
     }
 }
